91595446284b8f59707628fc3e4aaf11c80cc0cd
John <montemarjohn66@gmail.com>
2024-08-18 22:22:13 +0800

    added ngrok conditions

--- backend/helpers.php
+++ backend/helpers.php
@@ -23,6 +23,8 @@ class Helpers
 
     if ($_SERVER['HTTP_HOST'] == "localhost") {
       define("SERVER_NAME", "http://$_SERVER[SERVER_NAME]/job-portal");
+    } else if (str_contains($_SERVER['HTTP_HOST'], "ngrok-free.app")) {
+      define("SERVER_NAME", "https://$_SERVER[SERVER_NAME]/job-portal");
     } else {
       define("SERVER_NAME", "http://$_SERVER[SERVER_NAME]");
     }
